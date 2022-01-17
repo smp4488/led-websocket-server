fdbd6c52230c92bb7f361b71135a3f02c2c0f70b
smp4488 <smp4488@aol.com>
2022-01-16 23:00:27 -0500

    testing socket event

--- server.py
+++ server.py
@@ -2,7 +2,8 @@ from led import set_color_hex
 import eventlet
 import socketio
 
-sio = socketio.Server()
+# sio = socketio.Server()
+sio = socketio.Server(cors_allowed_origins='*')
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
