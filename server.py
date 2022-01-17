19195f91dd018c56bb1a13a5bc1862fc80a851c5
smp4488 <smp4488@aol.com>
2022-01-16 23:22:10 -0500

    testing socket event

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ async def server(websocket, path):
         # await unregister(websocket)
 
 if __name__ == '__main__':
-  start_server = websockets.serve(server, "localhost", 5000)
+  start_server = websockets.serve(server, "0.0.0.0", 5000)
   asyncio.get_event_loop().run_until_complete(start_server)
   asyncio.get_event_loop().run_forever()
 
