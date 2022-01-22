0605f46aca5286a421a0df721d647fbfc16656e2
smp4488 <smp4488@aol.com>
2022-01-21 22:57:31 -0500

    setting up logging

---
+++ logger.py
@@ -0,0 +1,19 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+
+logFile = '/var/log/serverio.log'
+
+my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024, 
+                                 backupCount=2, encoding=None, delay=0)
+my_handler.setFormatter(log_formatter)
+my_handler.setLevel(logging.INFO)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# if (logger.hasHandlers()):
+#     logger.handlers.clear()
+
+logger.addHandler(my_handler)
