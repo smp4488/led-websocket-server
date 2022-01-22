c2cced124d0d256cd903b5204957fa253218319d
smp4488 <smp4488@aol.com>
2022-01-21 23:02:04 -0500

    setting up logging

--- serverio.py
+++ serverio.py
@@ -14,27 +14,6 @@ import visualization
 
 logger = logging.getLogger()
 
-# import logging
-# from logging.handlers import RotatingFileHandler
-
-# log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
-
-# logFile = '/var/log/serverio.log'
-
-# my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024, 
-#                                  backupCount=2, encoding=None, delay=0)
-# my_handler.setFormatter(log_formatter)
-# my_handler.setLevel(logging.INFO)
-
-# logger = logging.getLogger()
-# logger.setLevel(logging.INFO)
-
-# # if (logger.hasHandlers()):
-# #     logger.handlers.clear()
-
-# logger.addHandler(my_handler)
-
-
 CURRENT_COLOR = None
 
 sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins='*', logger=logger)
