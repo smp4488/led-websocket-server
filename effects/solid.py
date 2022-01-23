96c8c7dcae4fc0106bf0584f4b41607f05f0898c
smp4488 <smp4488@aol.com>
2022-01-23 16:19:38 -0500

    adding effects manager

--- effects/solid.py
+++ effects/solid.py
@@ -1,4 +1,5 @@
 from led import set_color_hex, colorWipe
+import asyncio
 import effects.effect
 
 class Solid(effects.effect.Effect):
@@ -11,7 +12,8 @@ class Solid(effects.effect.Effect):
     self.running = False
     set_color_hex('#000000')
 
-  def run(self, options):
+  async def run(self, options):
     self.logger.info('running solid ' + options['color'])
     if options['color']:
       set_color_hex(options['color'])
+    await asyncio.sleep(1)
